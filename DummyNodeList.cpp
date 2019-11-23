e259949ce0183c603be5e25b57f7bafe0b6c1dd6
Choyeon-17 <vtkohcawsmktv@gmail.com>
2019-11-23 22:37:05 +0900

    Dummy Node List

---
+++ DummyNodeList.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename TYPE> class Node
+{
+public:
+ TYPE data;
+ Node* next;
+};
+
+template <typename TYPE> class DummyNodeList
+{
+private:
+ Node <TYPE>* head;
+ Node <TYPE>* before;
+ Node <TYPE>* current;
+public:
+ DummyNodeList()
+ {
+  head = new Node <TYPE>;
+  head->data = NULL;
+  head->next = NULL;
+ }
+
+ bool FirstNode()
+ {
+  if (current)
+   return true;
+
+  return false;
+ }
+
+ void InsertNode(TYPE NewData)
+ {
+  Node <TYPE>* NewNode = new Node <TYPE>;
+  NewNode->data = NewData;
+  NewNode->next = NULL;
+
+  before = head;
+  current = head->next;
+
+  if (!FirstNode())
+   head->next = NewNode;
+
+  else
+  {
+   if (NewData <= current->data)
+   {
+    NewNode->next = before->next;
+    before->next = NewNode;
+   }
+
+   else
+   {
+    while (current->next && NewData > current->next->data)
+    {
+     before = current;
+     current = current->next;
+    }
+
+    if (current->next)
+    {
+     NewNode->next = current->next;
+     current->next = NewNode;
+    }
+
+    else
+     current->next = NewNode;
+   }
+  }
+ }
+
+ void DeleteNode(TYPE TargetData)
+ {
+  before = head;
+  current = head->next;
+
+  if (!FirstNode())
+   cout << "삭제할 노드가 없습니다." << endl;
+
+  else
+  {
+   while (current != NULL)
+   {
+    if (current->data == TargetData && current->next == NULL)
+     before->next = NULL;
+    else if (current->data == TargetData && current->next)
+     before->next = current->next;
+    else
+    {
+     before = current;
+     current = current->next;
+    }
+   }
+  }
+ }
+
+ void PrintList()
+ {
+  current = head->next;
+
+  while (current)
+  {
+   cout << current->data << " ";
+   current = current->next;
+  }
+
+  cout << endl << endl;
+ }
+};
+
+int main()
+{
+ int array[10] = { 1, 3, 2, 5, 6, 4, 9, 7, 8, 0 };
+
+ DummyNodeList<int> list;
+
+ for (int i = 0; i < 10; i++)
+  list.InsertNode(array[i]);
+
+ list.PrintList();
+
+ for (int i = 0; i < 10; i++)
+  list.DeleteNode(array[i]);
+}
