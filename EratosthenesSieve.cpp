1dcfc4dfd4949e4c00f75ecfe53e48144a607dea
Choyeon <VtKoHcAwSmKtV@gmail.com>
2019-12-18 23:06:39 +0900

    CREATE: Eratosthenes' sieve.cpp

--- EratosthenesSieve.cpp
+++ EratosthenesSieve.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main()
 {
     int n;
+    
     cin >> n;
+    
     bool *PrimeArray = new bool[n];
+    
     memset(PrimeArray, 0, n);
 
     for (int i = 2; i < n; i++)
